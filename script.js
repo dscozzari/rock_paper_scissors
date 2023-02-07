aa9c876ea4c521b6a33e25b08711e0f7ff89fdd6
Don Scozzari <donscozzari93@gmail.com>
2023-02-07 15:50:56 -0500

    Add play game function
    This adds a function ton play one round of the game. It prompts the user to enter rock, paper, or scissors and then puts it up against the computer choice. The console will then show both choices, and the winner.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let getComputerChoice = function () {
+const getComputerChoice = function () {
   // Generates random number from 1 to 3
   let choice = Math.floor(Math.random() * 3) + 1;
 
@@ -14,4 +14,41 @@ let getComputerChoice = function () {
   }
 };
 
-console.log(getComputerChoice());
+// play one round
+
+const playRound = function (playerChoice, computerChoice) {
+  playerChoice = prompt('Rock, paper, or scissors?').toLowerCase();
+  console.log('Player:', playerChoice);
+
+  computerChoice = getComputerChoice();
+  console.log('Computer:', computerChoice);
+
+  // If player chooses rock
+  if (playerChoice === 'rock' && computerChoice === 'rock') {
+    console.log('It is a DRAW!');
+  } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+    console.log('You WIN!, rock beats scissors!');
+  } else if (playerChoice === 'rock' && computerChoice === 'paper') {
+    console.log('You LOSE! Paper beats rock.');
+  }
+
+  // If player chooses scissors
+  if (playerChoice === 'scissors' && computerChoice === 'scissors') {
+    console.log('It is a DRAW!');
+  } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+    console.log('You WIN!, scissors beats paper!');
+  } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+    console.log('You LOSE! Rock beats scissors.');
+  }
+
+  // If player chooses paper
+  if (playerChoice === 'paper' && computerChoice === 'paper') {
+    console.log('It is a DRAW!');
+  } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+    console.log('You WIN!, paper beats rock!');
+  } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+    console.log('You LOSE! Scissors beats paper.');
+  }
+};
+
+playRound();
